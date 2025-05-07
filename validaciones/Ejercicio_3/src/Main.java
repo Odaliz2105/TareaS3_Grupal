d8c0310762b4bef8529c5ebe5ef9c6d2598eabf3
FelipeZapata137 <felipe.zapata@epn.edu.ec>
2025-05-07 00:39:46 -0500

    Main.java
    ...

--- validaciones/Ejercicio_3/src/Main.java
+++ validaciones/Ejercicio_3/src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+class Juego {
+    int puntaje;
+    int total;
+
+    public Juego(int puntajeInicial) {
+        this.puntaje = puntajeInicial;
+        this.total = 0;
+    }
+
+    public void agregarPuntaje(int puntosAAgregar) {
+        this.total += puntosAAgregar;
+        System.out.println("Se agregaron " + puntosAAgregar + " puntos. Puntaje total: " + this.total);
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner teclado = new Scanner(System.in);
+        int puntos;
+        Juego juegoActual = new Juego(0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("Ingrese el puntaje a agregar ( 0 para salir ): ");
+            puntos = teclado.nextInt();
+            teclado.nextLine();
+
+            if (puntos > 0) {
+                juegoActual.agregarPuntaje(puntos);
+            } else if (puntos == 0) {
+                System.out.println("Saliendo del sistema de ingreso de puntajes...");
+                break;
+            } else {
+                System.out.println("Error, no puede ingresar cantidades negativas. Intente de nuevo");
+            }
+        } while (puntos != 0);
+
+        System.out.print("Finalizado con un puntaje total de: " + juegoActual.getTotal());
+        teclado.close();
     }
 }
