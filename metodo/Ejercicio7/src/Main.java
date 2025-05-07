57809ec0b92aa397e537cb3c92bb9a047a987028
FelipeZapata137 <felipe.zapata@epn.edu.ec>
2025-05-07 00:37:15 -0500

    Main.java
    FZ

--- metodo/Ejercicio7/src/Main.java
+++ metodo/Ejercicio7/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+class Productos {
+    float precio;
+    float dscto;
+    float total;
+
+    public float calcularPrecioConDescuento(float precio, float dscto) {
+        this.precio = precio;
+        this.dscto = dscto;
+        System.out.println("Tiene un descuento del " + dscto + "%" + " equivalente a " + precio + "");
+        dscto /= 100;
+        this.total = precio * (1 - dscto);
+        return this.total;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner teclado = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("Ingrese el precio del producto (USD): ");
+        float precio = teclado.nextFloat();
+        teclado.nextLine();
+        System.out.println("Ingrese el descuento del producto ( n% ): ");
+        float dscto = teclado.nextFloat();
+        teclado.nextLine();
+        Productos Producto1 = new Productos();
+        Producto1.calcularPrecioConDescuento(precio, dscto);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("El valor total del producto es: " + df.format(Producto1.calcularPrecioConDescuento(precio, dscto)));
+        teclado.close();
     }
 }
