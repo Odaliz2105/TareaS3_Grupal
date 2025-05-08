b92b1841a2c8744ace43e914c7fa6f56b32e8dae
Ivanp2003 <andrespanchichavez@gmail.com>
2025-05-08 08:17:51 -0500

    Prueba de commit

--- encapsulamiento/Ejercicio 4/src/Main.java
+++ encapsulamiento/Ejercicio 4/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Crear objeto
-        Usuario usuario = new Usuario("Inglés", true);
+        Usuario usuario = new Usuario("Español", true);
 
         // Validar idioma inicial
         if (usuario.validarIdioma()) {
